22d98354599eb4ec3212562f07cb48e13f6e92cd
Tom Hardy <tomhardy@192.168.0.111>
2021-01-18 00:13:34 +0800

    Initial commit

---
+++ src/main/java/com/kingdoms/godfather/GodfatherApplication.java
@@ -0,0 +1,13 @@
+package com.kingdoms.godfather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodfatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodfatherApplication.class, args);
+    }
+
+}
